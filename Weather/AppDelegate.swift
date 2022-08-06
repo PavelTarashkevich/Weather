7388a16752205e675ae2bd0f252a596d801607fe
Alexey Bondar <alexey.bondar@apalon.com>
2022-08-06 23:47:26 +0300

    Fixed entry point

--- Weather/AppDelegate.swift
+++ Weather/AppDelegate.swift
@@ -10,21 +10,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
-  var coordinator: RootCoordinator!
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-    let initialWindow = UIWindow()
-      let useCaseProvider = UseCaseProvider()
-    let rootCoordinator = RootCoordinator(window: initialWindow,
-                                          useCaseProvider: useCaseProvider,
-                                          coordinatorProvider: RootCoordinatorProvider(useCaseProvider: useCaseProvider))
-    rootCoordinator.start()
-    initialWindow.makeKeyAndVisible()
-    window = initialWindow
-    coordinator = rootCoordinator
-
     return true
   }
 }
